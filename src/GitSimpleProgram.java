9a0b67c7ffeffbbd57970ea8bcaea9c93b4b13eb
Aniket-4545 <chuadhariam007>
2021-11-23 23:38:47 +0530

    Hi priyanka I am adding one println line plz check my code.

--- src/GitSimpleProgram.java
+++ src/GitSimpleProgram.java
@@ -4,7 +4,7 @@ public class GitSimpleProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Everyone...");
-
+System.out.println(" hello priyanka kharade");
 	}
 
 }
